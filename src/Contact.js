a49d96cdbef4adb4692937d358f1e55612b1c5db
Maheeb <maheeb009@gmail.com>
2020-05-18 22:28:11 +0600

    Event

--- src/Contact.js
+++ src/Contact.js
@@ -5,11 +5,19 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types'
 import './contact.css'
 class Contact extends Component {
+    state ={  };
+    onShowClick = (name,e)=>{
+        console.log(name)
+    }
+
+
    render() {
        const {name,address,phone} = this.props.item;
        return (
            <div className="card card-body mb-3">
-                <h4>{name}</h4>
+                <h4>{name} <i
+                    onClick={this.onShowClick.bind(this,name)}
+                    className="fa fa-sort-down" /> </h4>
                <ul className="list-group">
                    <li className="list-group-item">Address: {address} </li>
                    <li className="list-group-item">Phone No: {phone}  </li>
